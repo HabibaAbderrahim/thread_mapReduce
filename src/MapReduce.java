ecc5cb6bf60e33134f9aeb2105fe03fc33176bc7
HabibaAbderrahim <heububabiba@gmail.com>
2022-12-10 22:30:30 +0100

    execution time

--- src/MapReduce.java
+++ src/MapReduce.java
@@ -4,9 +4,13 @@ public class MapReduce {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException  {
 
+        long startTime = System.currentTimeMillis();
         //array initialisation with 1000 element
         int[] array = CallableObject.getArray(1000);
         mapReduce(array);
+        long endTime = System.currentTimeMillis();
+        //print time
+        System.out.println("Total elapsed time in execution of method is :"+ ((double)(endTime-startTime)/100000) + "ms");
 
 
 
