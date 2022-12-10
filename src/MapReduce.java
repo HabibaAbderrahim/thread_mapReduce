a5877298869d93d1574e0b7d9e6bf233dd46e09d
HabibaAbderrahim <heububabiba@gmail.com>
2022-12-10 22:20:14 +0100

    main run MapReduce

--- src/MapReduce.java
+++ src/MapReduce.java
@@ -1,2 +1,19 @@
+import java.util.concurrent.ExecutionException;
+
 public class MapReduce {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException  {
+
+        //array initialisation with 1000 element
+        int[] array = CallableObject.getArray(1000);
+        mapReduce(array);
+
+
+
+    }
+       //call map task and reduce task
+    public static void mapReduce(int[] arr) throws InterruptedException, ExecutionException {
+        long[] mapOutput = Map.mapTask(arr);//10 threads, 0 chunks each with 100elts --> Result of each chunk stored in arr mapOut
+        Reduce.reduceTask(mapOutput);// sum up data from the array of mapTask of each chunk
+    }
 }
