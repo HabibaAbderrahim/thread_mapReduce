01f4bec7bcde069187ac7e23d0c23b151d9205dd
HabibaAbderrahim <heububabiba@gmail.com>
2022-12-10 22:10:29 +0100

    reduce task

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,12 @@
 public class Reduce {
+
+    public static void reduceTask(long[] arr) {
+        long sum = 0;
+        //for ecah elemnt in array arr
+        for(long x : arr) {
+            sum += x;
+        }
+
+        System.out.println("Value is " + sum);
+    }
 }
